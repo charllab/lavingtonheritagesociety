28a911f5f8bd8352d79ab004a0df8dd9068508f5
charllab <developer.charl@gmail.com>
2022-12-15 22:50:50 +0200

    build continued

--- src/themes/lavingtonheritagesociety/front-page.php
+++ src/themes/lavingtonheritagesociety/front-page.php
@@ -1,5 +1,217 @@
 <?php get_header(); ?>
 
+    <section class="hero-section bg-dark" id="top">
+        <div class="container py-2 px-1 text-center">
+            <h2 class="display-4 ff-lobster text-white mb-1 mb-lg-150">Coldstream Lavington Heritage Society</h2>
+            <a href="#" class="btn btn-light">Donate Now</a>
+        </div><!-- container -->
+    </section><!-- #top -->
+
+    <section class="bg-primary" id="our-purpose">
+        <div class="container py-2 px-1 text-center text-white">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <h2 class="display-4 ff-lobster text-white">Our Purpose</h2>
+                    <p>To protect and display the area’s unique history, and to tell the stories of the families
+                        who were early settlers in this area.</p>
+                </div><!-- col -->
+            </div><!-- row -->
+        </div><!-- container -->
+    </section><!-- #our-purpose -->
+
+    <section class="bg-white" id="projects">
+        <div class="container py-2 px-1 text-center">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <h2 class="display-4 ff-lobster mb-0">Projects</h2>
+                </div><!-- col -->
+            </div><!-- row -->
+        </div><!-- container -->
+    </section><!-- #projects -->
+
+    <section class="bg-white" id="projects">
+        <div class="container px-0">
+            <div class="row justify-content-center align-items-center no-gutters">
+                <div class="col-lg-6">
+                    <div class="position-relative">
+                        <img src="https://placehold.co/1200x900" alt="" class="img-fluid d-block">
+                        <div class="position-absolute title-overlay text-center">
+                            <p class="mb-0 small py-50">Coldstream Elementary School, 1934</p>
+                        </div>
+                    </div><!-- position-relative -->
+                </div><!-- col -->
+                <div class="col-lg-6 mobile-gutter-fix">
+                    <div class="py-1 py-lg-0 px-lg-2">
+                        <h2>Current Projects</h2>
+                        <p>We are currently finalizing the set up of our Society which includes the development of our
+                            operational procedures as well as arranging for project leaders, developing project details
+                            and
+                            working on acquiring funding for upcoming projects.</p>
+                    </div>
+                </div><!-- col -->
+            </div><!-- row -->
+        </div><!-- container -->
+    </section><!-- #projects -->
+
+    <section class="bg-white" id="upcoming">
+        <div class="container px-0">
+            <div class="row justify-content-center align-items-center no-gutters">
+                <div class="col-lg-6 order-lg-1">
+                    <div class="position-relative">
+                        <img src="https://placehold.co/1200x900" alt="" class="img-fluid d-block">
+                        <div class="position-absolute title-overlay text-center">
+                            <p class="mb-0 small py-50">Digging the Grey Canal, 1914</p>
+                        </div>
+                    </div><!-- position-relative -->
+                </div><!-- col -->
+                <div class="col-lg-6 order-lg-0 mobile-gutter-fix">
+                    <div class="py-1 py-lg-0 px-lg-2">
+                        <h2>Upcoming Projects</h2>
+                        <ul>
+                            <li>Ancestral First Nations use of and travel through the area</li>
+                            <li>Collation of information on monuments, buildings, and other manmade historical
+                                features
+                            </li>
+                            <li>Enumeration and biography of people who have been part of the development of
+                                Coldstream
+                            </li>
+                            <li>The collation of the origin of place names, streets, roads, etc.</li>
+                            <li>Geography of the Coldstream area with details on features that are readily observable
+                                today
+                            </li>
+                            <li>Videos of family histories</li>
+                            <li>Industrial and agricultural businesses that have existed in the area</li>
+                        </ul>
+                    </div>
+                </div><!-- col -->
+            </div><!-- row -->
+        </div><!-- container -->
+    </section><!-- #upcoming -->
+
+    <section class="bg-white" id="previuos">
+        <div class="container px-0">
+            <div class="row justify-content-center align-items-center no-gutters">
+                <div class="col-lg-6">
+                    <div class="position-relative">
+                        <img src="https://placehold.co/1200x900" alt="" class="img-fluid d-block">
+                        <div class="position-absolute title-overlay text-center">
+                            <p class="mb-0 small py-50">Vernon Preparatory School Chapel, 2022</p>
+                        </div>
+                    </div><!-- position-relative -->
+                </div><!-- col -->
+                <div class="col-lg-6 mobile-gutter-fix">
+                    <div class="py-1 py-lg-0 px-lg-2">
+                        <h2>Previous Projects</h2>
+                        <p>Roof replacement of the Vernon Preparatory School Chapel (1914-1966) which contains many
+                            important heritage artifacts. Funded by donations and completed in 2022.</p>
+                    </div>
+                </div><!-- col -->
+            </div><!-- row -->
+        </div><!-- container -->
+    </section><!-- #previuos -->
+
+    <section class="bg-primary" id="about">
+        <div class="container pt-3 pb-1 px-1 text-center text-white">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <h2 class="display-4 ff-lobster text-white">Who We Are</h2>
+                    <p>The Coldstream Lavington Heritage Society was formed in 2019 by long-time residents who wished to
+                        record and preserve the unique local history and recollections of this area before they are
+                        lost.ue history, and to tell the stories of the families who were early settlers in this
+                        area.</p>
+                </div><!-- col -->
+            </div><!-- row -->
+        </div><!-- container -->
+        <div class="container pt-2 pb-1 px-1 text-center text-white">
+            <div class="row justify-content-center">
+                <div class="col-xl-10">
+                    <h2 class="text-white mb-150">Directors</h2>
+                </div><!-- col -->
+            </div><!-- row -->
+            <div class="row justify-content-center">
+                <div class="col-lg-10 border border-danger">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-4 col-md-6 text-center">
+                            <div class="px-2">
+                                <img src="https://placehold.co/548x548" alt=""
+                                     class="rounded-circle img-fluid d-block mb-150 profile-img mx-auto">
+                                <p class="h5 text-white mb-0">Ted Osborn</p>
+                                <p class="small">President</p>
+                            </div><!-- px -->
+                        </div><!-- col -->
+                        <div class="col-lg-4 col-md-6 text-center">
+                            <img src="https://placehold.co/548x548" alt=""
+                                 class="rounded-circle img-fluid d-block mb-150 profile-img mx-auto">
+                            <p class="h5 text-white mb-0">Ted Osborn</p>
+                            <p class="small">President</p>
+                        </div><!-- col -->
+                        <div class="col-lg-4 col-md-6 text-center">
+                            <img src="https://placehold.co/548x548" alt=""
+                                 class="rounded-circle img-fluid d-block mb-150 profile-img mx-auto">
+                            <p class="h5 text-white mb-0">Ted Osborn</p>
+                            <p class="small">President</p>
+                        </div><!-- col -->
+                    </div><!-- row -->
+                </div><!-- col -->
+            </div><!-- row -->
+        </div><!-- container -->
+        <div class="container pt-1 pb-2 px-1 text-center text-white">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <h2 class="text-white mb-150">Members At Large</h2>
+                </div><!-- col -->
+            </div><!-- row -->
+            <div class="row justify-content-center">
+                <div class="col-lg-10 border border-danger">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-4 col-md-6 text-center">
+                            <img src="https://placehold.co/548x548" alt=""
+                                 class="rounded-circle img-fluid d-block mb-150 profile-img mx-auto">
+                            <p class="h5 text-white mb-0">Ted Osborn</p>
+                            <p class="small">President</p>
+                        </div><!-- col -->
+                        <div class="col-lg-4 col-md-6 text-center">
+                            <img src="https://placehold.co/548x548" alt=""
+                                 class="rounded-circle img-fluid d-block mb-150 profile-img mx-auto">
+                            <p class="h5 text-white mb-0">Ted Osborn</p>
+                            <p class="small">President</p>
+                        </div><!-- col -->
+                        <div class="col-lg-4 col-md-6 text-center">
+                            <img src="https://placehold.co/548x548" alt=""
+                                 class="rounded-circle img-fluid d-block mb-150 profile-img mx-auto">
+                            <p class="h5 text-white mb-0">Ted Osborn</p>
+                            <p class="small">President</p>
+                        </div><!-- col -->
+                    </div><!-- row -->
+                </div><!-- col -->
+            </div><!-- row -->
+        </div><!-- container -->
+    </section><!-- #about -->
+
+    <section class="bg-white" id="gallery">
+        <div class="container py-2 px-1 text-center">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <h2 class="display-4 ff-lobster mb-0">Gallery</h2>
+                </div><!-- col -->
+            </div><!-- row -->
+        </div><!-- container -->
+    </section><!-- #projects -->
+
+    <section class="bg-primary" id="our-purpose">
+        <div class="container py-2 px-1 text-center text-white">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <h2 class="display-4 ff-lobster text-white">How You Can Help</h2>
+                    <p>If your family settled in the area prior to 1950, we’d love to hear your story! We always welcome
+                        new volunteers who can help bring our history to life.</p>
+                    <p>We are also grateful for your tax-deductible financial gift to help us reach our goals.</p>
+                    <a href="#" class="btn btn-secondary mt-1">Get Involved</a>
+                </div><!-- col -->
+            </div><!-- row -->
+        </div><!-- container -->
+    </section><!-- #our-purpose -->
+
     <section>
         <div class="container">
             <div class="row">
@@ -19,7 +231,7 @@
                         similique!</p>
                     <a href="#">Hamburgefonstiv</a>
                     <div>
-                        <a href="#" class="btn btn-primary">Hamburgefonstiv</a>
+                        <a href="#" class="btn btn-light">Hamburgefonstiv</a>
                     </div>
                     <div>
                         <a href="#" class="btn btn-secondary">Hamburgefonstiv</a>
