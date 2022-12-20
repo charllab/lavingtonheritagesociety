005836a6abb835494f5f376ad9043dc2065df81d
charllab <developer.charl@gmail.com>
2022-12-20 21:25:35 +0200

    fine tuned sticky menu and updated the gallery titles

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,12 @@
 jQuery(function () {
 
+    // close scroll to menu
+    // https://stackoverflow.com/questions/42401606/how-to-hide-collapsible-bootstrap-navbar-on-click
+    //https://www.codeply.com/go/nFDHoEqqJQ
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    });
+
     // Lightbox gallery
     jQuery('.js-gallery').magnificPopup({
         delegate: 'a',
--- src/themes/lavingtonheritagesociety/partials/header/hero-nav-overlay.php
+++ src/themes/lavingtonheritagesociety/partials/header/hero-nav-overlay.php
@@ -1,6 +1,6 @@
 <header id="header" class="hero-nav-overlay">
 
-    <nav class="navbar navbar-expand-md navbar-dark bg-secondary px-50 px-md-0">
+    <nav class="navbar navbar-expand-md navbar-dark bg-secondary px-50 px-md-0 z-index-500">
         <div class="container">
             <div class="nav-logo">
                 <a href="<?php echo esc_url(home_url('/')); ?>">
@@ -11,7 +11,8 @@
                 </a>
             </div>
 
-            <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target=".mainnav-m" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target=".mainnav-m"
+                    aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fas fa-bars"></i>
             </button>
 
@@ -28,37 +29,37 @@
             </div>
         </div>
     </nav>
-
-
-    <div id="scrollingNav" class="bg-secondary">
-    <nav class="navbar navbar-expand-md navbar-dark bg-secondary px-50 px-md-0">
-        <div class="container">
-            <div class="nav-logo">
-                <a href="<?php echo esc_url(home_url('/')); ?>">
-                    <img src="<?php bloginfo('template_url'); ?>/images/logo.svg"
-                         alt="<?php bloginfo('name'); ?> - Logo"
-                         class="img-fluid">
-                    <span class="sr-only"><?php bloginfo('name'); ?></span>
-                </a>
-            </div>
-
-            <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target=".mainnav-m" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fas fa-bars"></i>
-            </button>
-
-            <div class="mainnav-m collapse navbar-collapse d-md-flex flex-md-column">
-                <?php wp_nav_menu([
-                    'theme_location' => 'primary',
-                    'container_class' => 'ml-md-auto',
-                    'container_id' => 'mainnav',
-                    'menu_class' => 'navbar-nav ml-md-auto',
-                    'fallback_cb' => '',
-                    'menu_id' => 'main-menu',
-                    'walker' => new understrap_WP_Bootstrap_Navwalker(),
-                ]); ?>
-            </div>
-        </div>
-    </nav>
-    </div><!-- scrollingNav -->
-
 </header>
+
+<div id="scrollingNav" class="hero-nav-overlay">
+    <nav class="navbar navbar-expand-md navbar-dark bg-secondary px-50 px-md-0">
+        <div class="container">
+            <div class="nav-logo">
+                <a href="<?php echo esc_url(home_url('/')); ?>">
+                    <img src="<?php bloginfo('template_url'); ?>/images/logo.svg"
+                         alt="<?php bloginfo('name'); ?> - Logo"
+                         class="img-fluid">
+                    <span class="sr-only"><?php bloginfo('name'); ?></span>
+                </a>
+            </div>
+
+            <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target=".mainnav-m"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+
+            <div class="mainnav-m collapse navbar-collapse d-md-flex flex-md-column">
+                <?php wp_nav_menu([
+                    'theme_location' => 'primary',
+                    'container_class' => 'ml-md-auto',
+                    'container_id' => 'mainnav',
+                    'menu_class' => 'navbar-nav ml-md-auto',
+                    'fallback_cb' => '',
+                    'menu_id' => 'main-menu',
+                    'walker' => new understrap_WP_Bootstrap_Navwalker(),
+                ]); ?>
+            </div>
+        </div>
+    </nav>
+</div><!-- scrollingNav -->
+
